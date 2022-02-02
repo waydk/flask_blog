38ca0b9ce71beac7ba8b5e8149534dec903151df
waydk <waydk.note@gmail.com>
2022-02-02 23:44:29 +0800

    1.0.1: added the ability to select a movie

--- blog/routes.py
+++ blog/routes.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from environs import Env
 from flask import render_template, request, redirect, url_for
 from flask import current_app as app
@@ -6,6 +7,8 @@ import smtplib
 from blog.admin_views import AuthException
 
 from .models import Articles
+import requests
+from bs4 import BeautifulSoup
 
 env = Env()
 env.read_env()
@@ -23,12 +26,22 @@ def send_email(name, email, phone, message):
         connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
     """Route for the homepage
     """
     articles = Articles.query.order_by(Articles.date.desc()).limit(4).all()
-    return render_template("index.html", articles=articles)
+    if request.method == 'POST':
+        try:
+            anime_title = request.form.get('anime-title')
+            r = requests.get(f'https://www.kinopoisk.ru/index.php?kp_query={anime_title}')
+            soup = BeautifulSoup(r.text, 'lxml')
+            most_wanted = soup.find('div', class_='element most_wanted')
+            data_id = most_wanted.find('div', class_='info').find('p', class_='name').find('a').get('data-id')
+            return  render_template("index.html", articles=articles, kinopoisk_id=data_id)
+        except Exception:
+            return render_template("index.html", articles=articles, kinopoisk_id=None)
+    return render_template("index.html", articles=articles, kinopoisk_id=None)
 
 
 @app.route('/logout')
